f0799b5f95730ef714fc294857d1367d0bf7905a
josh <hey@imjosh.dev>
2025-07-02 10:10:34 -0500

    feat: k8 autocompletion

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -67,6 +67,22 @@ return {
     local lspconfig = require("lspconfig")
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    lspconfig.yamlls.setup({
+      settings = {
+        yaml = {
+          schemas = {
+            kubernetes = "k*-*.yaml",
+            ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+            ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+            ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+            ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+            ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+            ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+            ["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+          },
+        },
+      },
+    })
     lspconfig.pyright.setup({})
     vim.lsp.config("*", {
       capabilities = capabilities,
